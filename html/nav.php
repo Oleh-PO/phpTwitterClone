89f164ffd194fa083baca21f88efa2af79db6cbc
Oleh-PO <oleh.po.2007@gmail.com>
2025-04-01 22:32:29 +0300

    01.04.2025

---
+++ html/nav.php
@@ -0,0 +1,19 @@
+<div class="navSearch">
+	<div class="searchInput">
+		<label name="search">🔍</label>
+		<input name="search" type="text">
+	</div>
+</div>
+<div class="navMain">
+	<div class="account">
+		<button>LOGIN</button>
+		<button>SINGUP</button>
+	</div>
+	<div class="discover">
+		<ul>
+			<li>HOME</li>
+			<li>FRIANDS</li>
+			<li>SETTINGS</li>
+		</ul>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>TEST</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<nav>
+		<?php require 'html/nav.php'; ?>
+	</nav>
+	<main>
+		<div class="container">
+		</div>
+	</main>
+</body>
+</html>
